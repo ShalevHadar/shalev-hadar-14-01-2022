c67cad74c9f413fc4235e106d6e614190d2d05c3
Shalev Hadar <shdar94@gmail.com>
2022-01-14 22:46:52 +0200

    added debounce function for autocomplete

--- src/search/search.js
+++ src/search/search.js
@@ -1,31 +1,48 @@
 import { Autocomplete, TextField } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import * as accuweather from "../api/accuweather";
 import { clearActiveCity, setActiveCity } from "../city/city-slice";
+import { useDebounceTime } from "../components/debounce";
 
 export function Search() {
   const [options, setOptions] = useState([]);
+  const [val, setDebounced] = useDebounceTime(450);
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    if (val) {
+      getAutocompleteCities(val);
+    } else {
+      return;
+    }
+  }, [val]);
+
+  const getAutocompleteCities = async (value) => {
+    if (!value) {
+      return;
+    }
+    const response = await accuweather.autocomplete(value);
+    const nextOptions = response.data.map((location) => {
+      return {
+        id: location.Key,
+        name: location.LocalizedName,
+        area: location.AdministrativeArea.LocalizedName,
+        country: location.Country.LocalizedName,
+      };
+    });
+    setOptions(nextOptions);
+  };
+
+  // on input, get autocomplete by the value
   const handleInputChange = async (event, value, reason) => {
     if (reason === "input") {
-      const response = await accuweather.autocomplete(value);
-      const nextOptions = response.data.map((location) => {
-        return {
-          id: location.Key,
-          name: location.LocalizedName,
-          area: location.AdministrativeArea.LocalizedName,
-          country: location.Country.LocalizedName,
-        };
-      });
-      setOptions(nextOptions);
+      setDebounced(value);
     }
   };
 
+  // after pick, move city (key & value to )
   const handleChange = (event, city, reason) => {
-    //console.log(value, reason);
-
     const handlers = {
       selectOption: () => {
         if (city) {
@@ -45,10 +62,11 @@ export function Search() {
   return (
     <div>
       <Autocomplete
-        id="free-solo-demo"
-        freeSolo
-        options={options.map((option) => option)}
-        getOptionLabel={(option) => `${option.name}`}
+        id="search-city-input"
+        options={options}
+        getOptionLabel={(option) =>
+          `${option.name}, ${option.country} (${option.area})`
+        }
         renderInput={(params) => (
           <TextField {...params} placeholder="Enter city" />
         )}
