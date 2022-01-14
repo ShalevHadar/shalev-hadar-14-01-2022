ffd078aea06be17f38b195d5fec11c3b53e2ef26
Shalev Hadar <shdar94@gmail.com>
2022-01-14 21:49:00 +0200

    Add search component to layout

--- src/home-page/home-page.js
+++ src/home-page/home-page.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Layout } from "../components/layout";
+import { Search } from "../search/search";
 
 export function Homepage() {
-  return <Layout>Body of the page</Layout>;
+  return (
+    <Layout>
+      <Search />
+    </Layout>
+  );
 }
