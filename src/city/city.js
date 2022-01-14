e8c17bebdbaaf8259b01756bc2584c5d4dace42a
Shalev Hadar <shdar94@gmail.com>
2022-01-15 00:26:19 +0200

    handling 5 days forecast

--- src/city/city.js
+++ src/city/city.js
@@ -1,14 +1,19 @@
 import { CircularProgress } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { getCurrentWeather } from "../api/accuweather";
+import {
+  getCurrentWeather,
+  getFiveDaysDailyForecast,
+} from "../api/accuweather";
 import { CityDetails } from "./city-details";
+import { Forecasts } from "./forecasts";
 
 export function City() {
   const [currentWeather, setCurrentWeather] = useState();
+  const [forecasts, setForecasts] = useState();
   const activeCity = useSelector((state) => state.city.activeCity);
   const activeCityId = activeCity?.id;
-  const loading = !currentWeather;
+  const loading = !currentWeather || !forecasts;
 
   // getting current city weather
   useEffect(() => {
@@ -30,13 +35,44 @@ export function City() {
     }
   }, [activeCityId]);
 
+  // getting 5 days forecast
+  useEffect(() => {
+    const updateForecast = async () => {
+      const {
+        data: { DailyForecasts: forecasts },
+      } = await getFiveDaysDailyForecast(activeCityId);
+      setForecasts(
+        forecasts.map((forecast) => ({
+          date: new Date(forecast.Date),
+          temperature: {
+            min: forecast.Temperature.Minimum.Value,
+            max: forecast.Temperature.Maximum.Value,
+          },
+          dayIcon: forecast.Day.Icon,
+          nightIcon: forecast.Night.Icon,
+        }))
+      );
+    };
+    if (activeCityId) {
+      updateForecast();
+    }
+  }, [activeCityId]);
+
   if (loading) {
     return <div>{activeCityId ? <CircularProgress /> : ""}</div>;
   }
 
   return (
     <div>
-      <CityDetails city={activeCity} currentWeather={currentWeather} />
+      <header>
+        <CityDetails city={activeCity} currentWeather={currentWeather} />
+      </header>
+      <div>
+        <h2>{currentWeather.description}</h2>
+      </div>
+      <div>
+        <Forecasts forecasts={forecasts} />
+      </div>
     </div>
   );
 }
