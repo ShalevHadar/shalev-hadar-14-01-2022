04282c2459937f312830a3135399e1f4a059efd9
Shalev Hadar <shdar94@gmail.com>
2022-01-15 17:13:21 +0200

    added favorite toggle button with logic

---
+++ src/components/toggle-favorite-button.js
@@ -0,0 +1,42 @@
+import React from "react";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import { useSelector, useDispatch } from "react-redux";
+import { addToFavorites, removeFromFavorites } from "../city/city-slice";
+
+export function ToggleFavorite({ cityId }) {
+  const favorites = useSelector((state) => state.city.favorites);
+  const dispatch = useDispatch();
+  const isKeyInsideFavorites = favorites.includes(cityId);
+
+  const handleClick = () => {
+    if (!isKeyInsideFavorites) {
+      dispatch(addToFavorites(cityId));
+    } else {
+      dispatch(removeFromFavorites(cityId));
+    }
+  };
+
+  return (
+    <Button
+      variant="contained"
+      onClick={handleClick}
+      style={{ textTransform: "none" }}
+      sx={{ height: 55 }}
+    >
+      {isKeyInsideFavorites ? (
+        <Box sx={{ display: "flex" }}>
+          <FavoriteBorderIcon sx={{ mr: 1 }} />
+          <Typography>Remove from Favorites</Typography>
+        </Box>
+      ) : (
+        <Box sx={{ display: "flex" }}>
+          <FavoriteIcon sx={{ mr: 1 }} />
+          <Typography>Add to Favorites</Typography>
+        </Box>
+      )}
+    </Button>
+  );
+}
