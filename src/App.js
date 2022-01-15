28940317fe36da8422e53ed8d688f32474c7aa79
Shalev Hadar <shdar94@gmail.com>
2022-01-15 13:36:06 +0200

    creating favorites page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Favorites } from "./favorites-page/favorites-page";
 import { Homepage } from "./home-page/home-page";
 import { store } from "./store";
 
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </BrowserRouter>
     </Provider>
--- src/api/accuweather.js
+++ src/api/accuweather.js
@@ -9,12 +9,12 @@ const apiClient = axios.create({
 });
 
 export async function autocomplete(query) {
-  const mock = autocompleteMock.filter(
-    (item) =>
-      item.LocalizedName.indexOf(query) ||
-      item.Country.LocalizedName.indexOf(query)
-  );
-  return { data: mock };
+  // const mock = autocompleteMock.filter(
+  //   (item) =>
+  //     item.LocalizedName.indexOf(query) ||
+  //     item.Country.LocalizedName.indexOf(query)
+  // );
+  // return { data: mock };
   const response = await apiClient.get("locations/v1/cities/autocomplete", {
     params: {
       q: query,
@@ -24,13 +24,13 @@ export async function autocomplete(query) {
 }
 
 export async function getCurrentWeather(key) {
-  return { data: cityMock };
+  // return { data: cityMock };
   const response = await apiClient.get(`currentconditions/v1/${key}`);
   return response;
 }
 
 export async function getFiveDaysDailyForecast(key, metric = true) {
-  return { data: fiveDaysDailyForecastMock };
+  // return { data: fiveDaysDailyForecastMock };
   const response = await apiClient.get(`forecasts/v1/daily/5day/${key}`, {
     params: {
       metric,
