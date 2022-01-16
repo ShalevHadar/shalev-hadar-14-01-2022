fbc9d470839f7a50d38208549f877a9695660d1f
Shalev Hadar <shdar94@gmail.com>
2022-01-16 09:47:48 +0200

    removed unsed files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 import { FavoritesPage } from "./favorites-page/favorites-page";
 import { Homepage } from "./home-page/home-page";
 import { store } from "./store";
