3572e57393bc8abb1b35281d821f8c30e1f9d3f9
Shalev Hadar <shdar94@gmail.com>
2022-01-17 21:18:37 +0200

    addeding dark-theme logic into navbar and app

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,13 +2,28 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { AppBar, Button, Container } from "@mui/material";
 import CloudIcon from "@mui/icons-material/Cloud";
-import { Box } from "@mui/system";
+import { Box, useTheme } from "@mui/system";
+import IconButton from "@mui/material/IconButton";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleDarkMode } from "../theme/theme-slice";
 
 export function Layout({ children }) {
+  const darkMode = useSelector((state) => state.theme.darkMode);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const theme = useTheme();
+  const toggleMode = () => {
+    dispatch(toggleDarkMode());
+  };
+
+  console.log(theme);
 
   return (
-    <Box>
+    <Box
+      sx={{ backgroundColor: theme.palette.background.default, height: "100%" }}
+    >
       <AppBar position="static">
         <Container
           style={{
@@ -24,19 +39,28 @@ export function Layout({ children }) {
           </Box>
           <Box>
             <Button
-              sx={{ textTransform: "none", color: "#fff" }}
+              sx={{ textTransform: "none" }}
               variant="text"
               onClick={() => navigate("/")}
+              color="inherit"
             >
               Home
             </Button>
             <Button
-              sx={{ textTransform: "none", color: "#fff" }}
+              sx={{ textTransform: "none" }}
               variant="text"
               onClick={() => navigate("/favorites")}
+              color="inherit"
             >
               Favorites
             </Button>
+            <IconButton
+              sx={{ textTransform: "none" }}
+              variant="text"
+              onClick={toggleMode}
+            >
+              {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
           </Box>
         </Container>
       </AppBar>
